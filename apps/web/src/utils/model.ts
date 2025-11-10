1b0c45e758824057c95d3abf7919da08ce734b27
sameera <www.sameera@gmail.com>
2025-11-10 12:41:51 -0500

    chore: minor changes

--- apps/web/src/utils/model.ts
+++ apps/web/src/utils/model.ts
@@ -1,15 +1,15 @@
 export interface Expectation {
-  level: number;
-  content: string;
-  description?: string;
+    level: number;
+    content: string;
+    description?: string;
 }
 
 export interface Competence {
-  name: string;
-  levels: Expectation[];
+    name: string;
+    levels: Expectation[];
 }
 
 export interface Category {
-  title: string;
-  competencies: Competence[];
+    title: string;
+    competencies: Competence[];
 }
